84c2c63fb738fbd2fb4765c6d4619d284de53625
markwcraddock <mark.w.craddock@gmail.com>
2017-04-08 09:49:39 +0100

    Update tm-alexa.user.js

--- tm-alexa.user.js
+++ tm-alexa.user.js
@@ -78,5 +78,9 @@ function removeWatch(item) {
 function makeDraggable() {
   $('.d-card-title').draggable();
 }
+    
+function addStyleSheets() {
+$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">');    
+}
 
 })();
