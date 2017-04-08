166b5aaff479e1ebbc0d1ac8a1f61040e189f357
markwcraddock <mark.w.craddock@gmail.com>
2017-04-08 08:31:58 +0100

    Create tm-alexa.user.js

---
+++ tm-alexa.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         Alexa Screen Modifier webVersion
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @require http://code.jquery.com/jquery-1.12.4.min.js
+// @require http://code.jquery.com/ui/1.8.21/jquery-ui.min.js
+// @require file:///var/www/tamper-monkey/jquery.ui.touch-punch.min.js
+// @author       mark craddock
+// @match        http://alexa.amazon.co.uk/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    var watchedObjectList = [];
+    
+    function watch(watchObject) {
+        watchedObjectList.push(watchObject);
+        if (typeof window.watchTimer == "undefined") {
+            window.watchTimer = setInterval(function() {
+                watchedObjectList.forEach(function(watchedItem) {
+                if ($(watchedItem.element).length) {
+                    var currentValue = $(watchedItem.element).html();
+                    if (watchedItem.oneTime === true) {
+                        removeWatch(watchedItem);
+                                 }
+                    if ((watchedItem.onUpdateOnly ===true && watchedItem.lastValue !== currentValue) || watchedItem.onUpdateOnly === false) {
+                        watchedItem.lastValue = currentValue;
+                        watchedItem.action(currentValue);
+                        }
+                }
+             });
+        }, 1000);
+    }
+    }
+
+
+var spokenCommandTracker = {element:'.playback-audio-text:first', oneTime: false, onUpdateOnly: true, action: function(value){console.log('performing action');spokenAction(value);}};    
+var mainTextTracker = {element:'.main-text ', oneTime: true, onUpdateOnly: false, action: switchToMain};
+var musicPlayTracker = {element: '#d-play-pause.play', oneTime: true, onUpdateOnly: false, action: switchToMain};
+var endMusicTracker = {element: '.d-overlay-text-wrapper', oneTime: true, onUpdateOnly: false, action: switchToMain}; 
+var menuButton = {element: '#navMenuIcon', oneTime: true, onUpdateOnly:false, action: makeDraggable};
+    
+watch(spokenCommandTracker);
+watch(musicPlayTracker);
+watch(endMusicTracker);
+watch(menuButton);
+
+function spokenAction(audioCommand){
+    console.log('command is:' + audioCommand);
+    var timerCommand = audioCommand.search("timer");
+    var timerCommandAlt = audioCommand.search("alarm");
+    if ((timerCommand !== -1) || (timerCommandAlt !== -1)) switchToTimer();
+}
+
+
+function switchToTimer() {
+    $('#iTimersAndAlarms').click();
+    $('.time-text').css({"font-size":"5rem"});
+    watch(mainTextTracker); 
+}
+    
+function switchToMain() {
+    if (window.location.href !== "http://alexa.amazon.co.uk/spa/index.html#cards") window.location = "http://alexa.amazon.co.uk/spa/index.html#cards";
+}
+    
+function removeWatch(item) {
+    console.log('removing item');
+    var index = watchedObjectList.indexOf(item);
+    if (index > -1) {
+        watchedObjectList.splice(index, 1);
+        
+    }
+}
+    
+function makeDraggable() {
+  $('#navMenuIcon').draggable();
+}
+
+})();
